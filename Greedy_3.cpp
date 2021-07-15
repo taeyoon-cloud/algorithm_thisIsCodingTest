70115df159ced3f9636d87005e6b8d20324daa36
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2021-07-15 18:17:33 +0900

    20210715_2

--- Greedy_3.cpp
+++ Greedy_3.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
 int main() {
-	vector<int> min_v; // 각 행에서의 최솟값을 저장하는 벡터
 	int n, m;
 	cin >> n >> m;
-
+	int max_value;
 	for (int i = 0; i < n; i++) {
-		vector<int> v; // 각 행의 값들을 저장하는 벡터
+		int min_value = 10001;
 		for (int j = 0; j < m; j++) {
-			int num;
-			cin >> num;
-			v.push_back(num);
+			int x;
+			cin >> x;
+			min_value = min(min_value, x);
 		}
-		sort(v.begin(), v.end()); // v벡터를 오름차순으로 정렬
-		min_v.push_back(v[0]); // 각 행에서의 최솟값만 min_v 벡터에 입력
+		max_value = max(max_value, min_value);
 	}
-	int max = *max_element(min_v.begin(), min_v.end());
-	// min_v 벡터의 최댓값 선언
 
-	cout << max << endl;
-
+	cout << max_value << endl;
 	return 0;
-	
+
 }
